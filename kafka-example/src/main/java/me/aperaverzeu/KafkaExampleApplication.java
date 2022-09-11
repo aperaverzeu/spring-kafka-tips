f95f9a6c2d737150e6c7edce93537f3d8735beb3
Aliaskei Peraverzeu <dev.mrojaczy@gmail.com>
2022-09-11 15:33:13 +0300

    rename folder

--- kafka-example/src/main/java/me/aperaverzeu/KafkaExampleApplication.java
+++ kafka-example/src/main/java/me/aperaverzeu/KafkaExampleApplication.java
@@ -1,4 +1,4 @@
-package me.aperaverzeu.kafkaexample;
+package me.aperaverzeu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- kafka-example/src/test/java/me/aperaverzeu/KafkaExampleApplicationTests.java
+++ kafka-example/src/test/java/me/aperaverzeu/KafkaExampleApplicationTests.java
@@ -1,4 +1,4 @@
-package me.aperaverzeu.kafkaexample;
+package me.aperaverzeu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
